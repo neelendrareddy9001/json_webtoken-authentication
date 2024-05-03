14345cf4bd6eb0d337220aa02f524a2ceb8c4d6c
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-05-03 08:29:19 +0200

    navbar is created with limited styles not responsive

---
+++ client/src/Components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='navbar flex justify-between'>
+        <a href='logo' className='logo'>navbar</a>
+        <ul className='flex justify-between'>
+            <li>
+                <a href='home'>Home</a>
+            </li>
+            <li>
+                <a href='about'>About</a>
+            </li>
+            <li>
+                <a href='blog'>Blog</a>
+            </li>
+            <li>
+                <a href='contact'>Contact</a>
+            </li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default Header
