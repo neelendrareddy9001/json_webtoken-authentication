39399357f50a95f980065d9d3f11d2deeaed6f45
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-05-03 08:44:57 +0200

    all pages created from the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
+import About from './pages/About/About';
+import Blog from './pages/Blog/Blog';
+import Contact from './pages/Contact/Contact'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path='*' exact element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -17,6 +17,9 @@ const Header = () => {
             <li>
                 <a href='contact'>Contact</a>
             </li>
+            <li>
+                <a href='login&sinin'>Login | Register</a>
+            </li>
         </ul>
 
     </div>
