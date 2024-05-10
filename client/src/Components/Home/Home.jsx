b1655bb0921bc0d115ffa9d44090ada649c7221a
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-05-10 23:13:40 +0200

    added styles successfully

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='home-container'>
-      <div >
+      <div className='flex justify-self-start' >
         <h4 className='heading'>This is about me</h4>
         <p>Here you can see the visula representation</p>
       </div>
