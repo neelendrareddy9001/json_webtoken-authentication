9f8b32ec606e39c93efe50bbaad2c1b16a27db84
neelendrareddy9001 <neelendrareddy123@gamil.com>
2024-05-12 23:43:39 +0200

    updated installed package

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {FiMenu} from 'react-icons/fi'
+import { IoClose } from "react-icons/io5";
 
 const Header = () => {
+    const [open, setMenuOpen] = useState(false)
+    const handleMenu = (e) => {
+        setMenuOpen(true)
+    }
   return (
     <>
     <nav className='navbar flex justify-between'>
         <a href='logo' className='logo'>navbar</a>
-        <ul className='flex justify-between'>
+        <ul className='flex justify-between menu'>
             <li>
                 <a href='home'>Home</a>
             </li>
@@ -27,7 +33,10 @@ const Header = () => {
                 </li>
             </div>
         </ul>
-
+        <div className='menu-icon'>
+            <FiMenu size={24} onClick={handleMenu}/>
+            <IoClose size={24} />
+        </div>
     </nav>
     <div className='menu_hover'></div>
     </>
