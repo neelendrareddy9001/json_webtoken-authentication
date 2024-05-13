30a07e8537ef29568b281f681f016f51d5fdd2af
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-05-13 23:26:06 +0200

    udpated successfully

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 const Home = () => {
   return (
     <section className='section-container'>
-      <div className='flex justify-self-start' >
+      <div className='home' >
         <h4 className='heading'>This is about me</h4>
-        <p>Here you can see the visula representation</p>
+        <p>Here you can see the visual representation of this project</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error magnam aut molestiae distinctio eos ea, neque perferendis corrupti beatae, doloremque fugit! Quibusdam, atque ipsam! Adipisci facilis autem architecto officiis rerum.</p>
+        <div className='btns'>
+          <button>Read More</button>
+          <button>Check Now</button>
+        </div>
       </div>
     </section>
   )
